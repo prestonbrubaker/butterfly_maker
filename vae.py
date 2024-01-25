082f47aef3894cae561799acad42ce755f84cf25
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-24 21:08:33 -0600

    Update vae.py

--- vae.py
+++ vae.py
@@ -152,7 +152,7 @@ else:
 
 
 # Loss and optimizer
-optimizer = optim.Adadelta(model.parameters(), lr=0.00001, eps=1e-8, weight_decay=0.00, amsgrad=False)
+optimizer = optim.Adadelta(model.parameters(), lr=0.00001, eps=1e-8, weight_decay=0.00)
 #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 
 
