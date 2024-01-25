4b678bd3c074c2b8ea16ed5a317c3925560235d3
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-24 19:51:55 -0600

    Update vae.py

--- vae.py
+++ vae.py
@@ -15,38 +15,35 @@ class VariationalAutoencoder(nn.Module):
         super(VariationalAutoencoder, self).__init__()
         self.latent_dim = latent_dim
 
-       # Encoder
+        # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),  # 
+            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1), 
             nn.ReLU(),
-            nn.Conv2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 64x64x64
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 128x32x32
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 256x16x16
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Flatten(),  # Flatten for linear layer input
-            nn.Linear(3*16*16, 1024),
+            nn.Flatten(),
+            nn.Linear(256 * 16 * 16, 1024),
             nn.ReLU()
         )
-
-        self.fc_mu = nn.Linear(1024, latent_dim)
-        self.fc_log_var = nn.Linear(1024, latent_dim)
 
         # Decoder
         self.decoder_input = nn.Linear(latent_dim, 1024)
 
         self.decoder = nn.Sequential(
-            nn.Linear(1024, 3*16*16),
+            nn.Linear(1024, 256 * 16 * 16),
             nn.ReLU(),
-            nn.Unflatten(1, (3, 16, 16)),  # Unflatten to 256x16x16 for conv transpose input
-            nn.ConvTranspose2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 128x32x32
+            nn.Unflatten(1, (256, 16, 16)),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 64x64x64
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(3, 3, kernel_size=4, stride=2, padding=1),  # Output: 32x128x128
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),  # 
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),  # Output: 3 channels for RGB
             nn.Sigmoid()
         )
 
